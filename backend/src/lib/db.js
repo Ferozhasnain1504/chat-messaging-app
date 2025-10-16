9338544b07a16d0ed1fa60a2de27f42b44c557b4
Feroz <ferozhasnain558@gmail.com>
2025-10-16 18:19:40 +0530

    welcome email implemented

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import {ENV} from '../lib/env.js';
 
 dotenv.config();
 
 export const connectDB = async () => {
     try {
-        const { MONGO_URI } = process.env;
+        const { MONGO_URI } = ENV;
         if(!MONGO_URI) throw new Error("MONGO_URI is not set");
         const conn = await mongoose.connect(MONGO_URI);
         console.log("MONGO CONNECTED:", conn.connection.host)
