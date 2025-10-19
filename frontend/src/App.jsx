df63abf15e0aeb8625a709436b0b8600b6a0704e
Feroz <ferozhasnain558@gmail.com>
2025-10-19 10:38:36 +0530

    frontend codebase setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Routes, Route } from "react-router";
+import ChatPage from "./pages/ChatPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import { useAuthStore } from "./store/useAuthStore.js";
 
-const App = () => {
+function App() {
+
+  const {authUser, login, isLoggedIn} = useAuthStore();
+  console.log("auth user: ", authUser);
+  console.log(" is Loggedin: ", isLoggedIn);
+  
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right, #4f4f4f2e_1px, transparent_1px), linear-gradient(to_bottom, #4f4f4f2e_1px, transparent_1px)] bg-[size:14px_24px]"></div>
+
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-30 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-30 blur-[100px]" />
+        
+      <button 
+       className="btn btn-primary z-10"
+       onClick={login}
+      >
+        Login
+      </button>
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+    
+  );
 }
 
-export default App
+export default App;
