b493520544f9ad8619f1ce5dc73158e2648cc390
Feroz <ferozhasnain558@gmail.com>
2025-10-20 22:04:57 +0530

    Chat Message Header implemented

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useChatStore } from '../store/useChatStore';
+import { useAuthStore } from '../store/useAuthStore.js';
+import ChatHeader from './ChatHeader.jsx';
+import { useEffect } from 'react';
 
 const ChatContainer = () => {
+
+  const {selectedUser, isMessagesLoading, getMessageByUserId, messages } = useChatStore();
+  const {authUser} = useAuthStore();
+
+  useEffect(() => {
+    if (selectedUser?._id) {
+      getMessageByUserId(selectedUser._id);
+    }
+  },[selectedUser, getMessageByUserId]);
+
   return (
-    <div>ChatContainer</div>
+    <>
+     <ChatHeader />
+    </>
   )
 }
 
